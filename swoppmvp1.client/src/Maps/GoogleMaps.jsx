c9ac4bd3e69abda51282d11843169c939659a377
Capt-Obv <49240674+Capt-Obv@users.noreply.github.com>
2024-10-26 15:07:08 +0200

    Got map to work

---
+++ swoppmvp1.client/src/Maps/GoogleMaps.jsx
@@ -0,0 +1,51 @@
+﻿import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+// The latitude of Oslo, Ostlandet, Norway is 59.911491, and the longitude is 10.757933.
+
+const center = {
+    lat: 59.911491,
+    lng: 10.757933
+};
+
+
+function GoogleMapsComponent() {
+    const api_key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: api_key
+    })
+  
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+  
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+        </GoogleMap>
+        ) : <></>
+}
+
+export default React.memo(GoogleMapsComponent)
--- swoppmvp1.client/src/routes/Content.jsx
+++ swoppmvp1.client/src/routes/Content.jsx
@@ -1,11 +1,17 @@
 import '@/App.css';
+import GoogleMapsComponent from "@/Maps/GoogleMaps.jsx";
 
 function Content() {
 
     return (
-        <div className="content" id={"content"}>
-            Content
-        </div>
+        <>
+            <div className="content" id={"content"}>
+                Content
+            </div>
+            <div className={"map"} id={"map"}>
+                <GoogleMapsComponent />
+            </div>
+        </>
     )
 }
 export default Content;
